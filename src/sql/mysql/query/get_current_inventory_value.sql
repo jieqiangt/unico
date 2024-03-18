fde5764803342f64906a3525b5bf847037feb369
Jie Qiang <jieqiangt@gmail.com>
2024-03-18 13:12:53 +0800

    - corrected database table name in sql query

--- src/sql/mysql/query/get_current_inventory_value.sql
+++ src/sql/mysql/query/get_current_inventory_value.sql
@@ -3,13 +3,13 @@ SELECT
     SUM(inv_qty) as 'current_inv_qty',
     SUM(inv_value) as 'current_inv_value'
 FROM
-    ft_warehouse_inventory_ts_new
+    ft_warehouse_inventory_ts
 WHERE
     as_of_date IN (
         SELECT
             MAX(as_of_date)
         FROM
-            ft_warehouse_inventory_ts_new
+            ft_warehouse_inventory_ts
     )
 GROUP BY
     pdt_code;
