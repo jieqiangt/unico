b629d41fbc646ecf32ee71b76e7a47fe04aa4dcf
Jie Qiang <jieqiangt@gmail.com>
2024-04-02 17:40:46 +0800

    - added local & overseas supplier dimension to daily purchase value ts

--- src/sql/mssql/init/ft_daily_purchase_value_ts.sql
+++ src/sql/mssql/init/ft_daily_purchase_value_ts.sql
@@ -1,12 +1,15 @@
 SELECT
     DATEFROMPARTS(YEAR(OPCH.DocDate), MONTH(OPCH.DocDate), DAY(OPCH.DocDate)) AS 'as_of_date',
+    COALESCE(OOND.IndDesc, 'LOCAL SUPPLIERS') AS 'local_overseas_ind',
     SUM(OPCH.DocTotal) AS 'amount_with_tax'
 FROM
     OPCH
+LEFT JOIN OCRD ON OPCH.CardCode = OCRD.CardCode
+LEFT JOIN OOND ON OCRD.IndustryC = OOND.IndCode
 WHERE
     OPCH.DocDate BETWEEN {{start_date}} AND {{end_date}}
     AND OPCH.CANCELED = 'N'
 GROUP BY
-    OPCH.DocDate
+    OPCH.DocDate, OOND.IndDesc
 ORDER BY
-    OPCH.DocDate
+    OPCH.DocDate, OOND.IndDesc
--- src/sql/mysql/create_table/ft_daily_purchase_value_ts.sql
+++ src/sql/mysql/create_table/ft_daily_purchase_value_ts.sql
@@ -1,7 +1,8 @@
 CREATE TABLE IF NOT EXISTS ft_daily_purchase_value_ts (
     as_of_date DATE NOT NULL,
+    local_overseas_ind VARCHAR(25) NOT NULL,
     amount_with_tax NUMERIC(10, 2) NOT NULL,
     created_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    PRIMARY KEY (as_of_date)
+    PRIMARY KEY (as_of_date, local_overseas_ind)
 );
