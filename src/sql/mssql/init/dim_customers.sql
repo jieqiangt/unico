80a6eb419b840f2c810dabb78ebb4a6c2e26d519
JQ\jieqiang <jieqiangt@gmail.com>
2024-04-24 13:02:52 +0800

    - added first purchase date & latest purchase date to dim suppliers table - added first sales date & latest sales date to dim cusomters table

--- src/sql/mssql/init/dim_customers.sql
+++ src/sql/mssql/init/dim_customers.sql
@@ -1,24 +1,51 @@
+WITH dim_customers AS (
+    SELECT
+        CardCode AS 'customer_code',
+        CardName AS 'name',
+        CmpPrivate AS 'entity_type',
+        CASE
+            WHEN OCRD.U_AF_CUSTGROUP = '' THEN OCRD.CardName
+            ELSE COALESCE (OCRD.U_AF_CUSTGROUP, OCRD.CardName)
+        END AS 'customer_group_name',
+        OSLP.SlpName AS 'sales_employee',
+        CONCAT(COALESCE(Address,''), ' ', COALESCE(Block,''), ' ', COALESCE(Building,''), ' ' , COALESCE(ZipCode,'')) AS 'address',
+        ZipCode AS 'zipcode',
+        OOND.IndName AS 'industry',
+        OCRG.GroupName AS 'trade_ind',
+        OCTG.PymntGroup AS 'payment_terms',
+        ValidFor AS 'is_active'
+    FROM
+        OCRD
+        LEFT JOIN OOND ON OCRD.IndustryC = OOND.IndCode
+        LEFT JOIN OCRG ON OCRD.GroupCode = OCRG.GroupCode
+        LEFT JOIN OCTG ON OCRD.GroupNum = OCTG.GroupNum
+        LEFT JOIN OSLP ON OCRD.SlpCode = OSLP.SlpCode
+    WHERE
+        CardType = 'C'
+        AND CardName IS NOT NULL
+),
+first_latest_sales_date AS (
+    SELECT
+        CardCode 'customer_code',
+        MIN(DocDate) AS 'first_sales_date',
+        MAX(DocDate) AS 'latest_sales_date',
+        CONCAT(
+            ROUND(DATEDIFF(day, MIN(DocDate), GETDATE()) / 365, 0),
+            ' Years',
+            ' & ',
+            DATEDIFF(day, MIN(DocDate), GETDATE()) % 365,
+            ' Days'
+        ) AS 'relationship_length'
+    FROM
+        ORDR
+    GROUP BY
+        CardCode
+)
 SELECT
-    CardCode AS 'customer_code',
-    CardName AS 'name',
-    CmpPrivate AS 'entity_type',
-    CASE
-        WHEN OCRD.U_AF_CUSTGROUP = '' THEN OCRD.CardName
-        ELSE COALESCE (OCRD.U_AF_CUSTGROUP, OCRD.CardName)
-    END AS 'customer_group_name',
-    OSLP.SlpName AS 'sales_employee',
-    [Address] AS 'address',
-    ZipCode AS 'zipcode',
-    OOND.IndName AS 'industry',
-    OCRG.GroupName AS 'trade_ind',
-    OCTG.PymntGroup AS 'payment_terms',
-    ValidFor AS 'is_active'
+    dim_customers.*,
+    first_sales_date,
+    latest_sales_date,
+    relationship_length
 FROM
-    OCRD
-    LEFT JOIN OOND ON OCRD.IndustryC = OOND.IndCode
-    LEFT JOIN OCRG ON OCRD.GroupCode = OCRG.GroupCode
-    LEFT JOIN OCTG ON OCRD.GroupNum = OCTG.GroupNum
-    LEFT JOIN OSLP ON OCRD.SlpCode = OSLP.SlpCode
-WHERE
-    CardType = 'C'
-    AND CardName IS NOT NULL
+    dim_customers
+    LEFT JOIN first_latest_sales_date ON dim_customers.customer_code = first_latest_sales_date.customer_code;
--- src/sql/mssql/init/dim_suppliers.sql
+++ src/sql/mssql/init/dim_suppliers.sql
@@ -1,18 +1,45 @@
+WITH dim_suppliers AS (
+    SELECT
+        CardCode AS 'supplier_code',
+        CardName AS 'name',
+        CmpPrivate AS 'entity_type',
+        CONCAT(COALESCE(Address,''), ' ', COALESCE(Block,''), ' ', COALESCE(Building,''), ' ' , COALESCE(ZipCode,'')) AS 'address',
+        ZipCode AS 'zipcode',
+        OOND.IndDesc AS 'overseas_local_ind',
+        OCRG.GroupName AS 'trade_ind',
+        OCTG.PymntGroup AS 'payment_terms',
+        ValidFor AS 'is_active'
+    FROM
+        OCRD
+        LEFT JOIN OOND ON OCRD.IndustryC = OOND.IndCode
+        LEFT JOIN OCRG ON OCRD.GroupCode = OCRG.GroupCode
+        LEFT JOIN OCTG ON OCRD.GroupNum = OCTG.GroupNum
+    WHERE
+        CardType = 'S'
+        AND CardName IS NOT NULL
+),
+first_last_purchase_date AS (
+    SELECT
+        CardCode AS 'supplier_code',
+        MIN(DocDate) AS 'first_purchase_date',
+        MAX(DocDate) AS 'latest_purchase_date',
+        CONCAT(
+            ROUND(DATEDIFF(day, MIN(DocDate), GETDATE()) / 365, 0),
+            ' Years',
+            ' & ',
+            DATEDIFF(day, MIN(DocDate), GETDATE()) % 365,
+            ' Days'
+        ) AS 'relationship_length'
+    FROM
+        OPCH
+    GROUP BY
+        CardCode
+)
 SELECT
-    CardCode AS 'supplier_code',
-    CardName AS 'name',
-    CmpPrivate AS 'entity_type',
-    [Address] AS 'address',
-    ZipCode AS 'zipcode',
-    OOND.IndDesc AS 'overseas_local_ind',
-    OCRG.GroupName AS 'trade_ind',
-    OCTG.PymntGroup AS 'payment_terms',
-    ValidFor AS 'is_active'
+    dim_suppliers.*,
+    first_purchase_date,
+    latest_purchase_date,
+    relationship_length
 FROM
-    OCRD
-    LEFT JOIN OOND ON OCRD.IndustryC = OOND.IndCode
-    LEFT JOIN OCRG ON OCRD.GroupCode = OCRG.GroupCode
-    LEFT JOIN OCTG ON OCRD.GroupNum = OCTG.GroupNum
-WHERE
-    CardType = 'S'
-    AND CardName IS NOT NULL
+    dim_suppliers
+    LEFT JOIN first_last_purchase_date ON dim_suppliers.supplier_code = first_sales_date.supplier_code;
