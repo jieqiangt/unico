95fe38d8525225b72efa25a14eef647f2b3e305f
JQ\jieqiang <jieqiangt@gmail.com>
2024-04-24 13:06:23 +0800

    - added customer group name, paid amount & oustanding amount in ar invoices

--- src/sql/mysql/create_table/ft_recent_ar_invoices.sql
+++ src/sql/mysql/create_table/ft_recent_ar_invoices.sql
@@ -6,10 +6,13 @@ CREATE TABLE IF NOT EXISTS ft_recent_ar_invoices (
     doc_date DATE NOT NULL,
     customer_code VARCHAR(25) NOT NULL,
     customer_name VARCHAR(255) NOT NULL,
+    customer_group_name VARCHAR(255) NOT NULL,
     customer_type VARCHAR(25) NOT NULL,
     sales_employee_code VARCHAR(25) NOT NULL,
     sales_employee_name VARCHAR(255) NOT NULL,
     amount_with_tax NUMERIC(10, 2),
+    paid_amount NUMERIC(10, 2),
+    outstanding_amount NUMERIC(10, 2),
     created_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_on DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     PRIMARY KEY (
