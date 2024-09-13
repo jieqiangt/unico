665dd7c781f20e6cb14d7b3b5c1c5b7aa29e6655
JQ\jieqiang <jieqiangt@gmail.com>
2024-09-13 15:03:00 +0800

    - added code for generating and updating sales invoices related tables

---
+++ src/sql/mssql/query/ft_daily_agg_sales_invoices.sql
@@ -0,0 +1,26 @@
+SELECT
+    DATEFROMPARTS(
+        YEAR(INV1.DocDate),
+        MONTH(INV1.DocDate),
+        DAY(INV1.DocDate)
+    ) AS 'doc_date',
+    DATEFROMPARTS(YEAR(INV1.DocDate), MONTH(INV1.DocDate), 1) AS 'start_of_month',
+    OINV.CardCode AS 'customer_code',
+    INV1.ItemCode AS 'pdt_code',
+    OINV.SlpCode AS 'sales_employee_code',
+    SUM(INV1.Quantity) AS 'qty',
+    AVG(INV1.Price) AS 'price',
+    SUM(INV1.LineTotal) AS 'amount'
+FROM
+    INV1
+    LEFT JOIN OINV ON INV1.DocEntry = OINV.DocEntry
+WHERE
+    INV1.DocDate BETWEEN {{start_date}} AND {{end_date}}
+    AND OINV.DocDate BETWEEN {{start_date}} AND {{end_date}}
+    AND OINV.Canceled = 'N'
+    AND INV1.ItemCode IS NOT NULL
+GROUP BY
+    INV1.DocDate,
+    OINV.CardCode,
+    INV1.ItemCode,
+    OINV.SlpCode;
